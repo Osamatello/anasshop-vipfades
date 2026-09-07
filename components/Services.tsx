d9c1460ba63d5a459ea57f30c41415c88d84089f
Osama Tello <aosamatello39@gmail.com>
2026-09-07 19:37:11 +0400

    Equalize compact service card heights

--- components/Services.tsx
+++ components/Services.tsx
@@ -62,7 +62,7 @@ export default function Services() {
           ))}
         </div>
 
-        <div className="mt-8 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 lg:gap-8">
+        <div className="mt-8 grid auto-rows-fr grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 lg:gap-8">
           {orderedServices.map((service: Service, index: number) => (
             <ServiceCard key={service.id} service={service} index={index} />
           ))}

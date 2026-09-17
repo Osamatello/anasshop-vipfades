67971047c9f8931cf6ace83dff4c4d457f4b06fe
Osama Tello <aosamatello39@gmail.com>
2026-09-17 13:24:40 +0400

    Use dedicated services page catalogue design

--- components/LeistungenContent.tsx
+++ components/LeistungenContent.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import Services from '@/components/Services';
+import LeistungenCatalogue from '@/components/LeistungenCatalogue';
 import { useReveal } from '@/lib/use-reveal';
 
 export default function LeistungenContent() {
   useReveal();
-  return <Services fullCatalogue />;
+  return <LeistungenCatalogue />;
 }

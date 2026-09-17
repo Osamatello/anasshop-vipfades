bc1232f3e0c3f9e505464512aa7aaed97f48996c
Osama Tello <aosamatello39@gmail.com>
2026-09-17 13:37:42 +0400

    Set reviews marquee speed to 55s

--- components/ClientExperiences.tsx
+++ components/ClientExperiences.tsx
@@ -151,11 +151,11 @@ export default function ClientExperiences() {
         }
 
         .reviews-track-left {
-          animation: reviews-scroll-left 65s linear infinite;
+          animation: reviews-scroll-left 55s linear infinite;
         }
 
         .reviews-track-right {
-          animation: reviews-scroll-right 65s linear infinite;
+          animation: reviews-scroll-right 55s linear infinite;
         }
 
         @keyframes reviews-scroll-left {
